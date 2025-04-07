036186c8064be402b6b6af6ce998575b8479b98d
johannaranki <jranki24@students.oamk.fi>
2025-04-07 13:31:25 +0300

    papukaijamerkkien lisääminen tulossivulle ja media gueryjen aloittaminen

--- js/tulokset.js
+++ js/tulokset.js
@@ -8,9 +8,29 @@ let flagResult = document.querySelector("#flag-result")
 let mapResult = document.querySelector("#map-result")
 let storyResult = document.querySelector("#story-result")
 
+let points = 0
+
+let bagdecount = 0
+
 showRecords()
 
 function showRecords() {
+
+    for (let i = 0; i < gameResults.length; i++) {
+        points += gameResults[i].tulos
+        if (gameResults[i].tulos == 10) {
+            bagdecount++
+        }
+    }
+
+    document.querySelector("#all-results").textContent = "Yhteispisteet " + points + " / 50"
+    document.querySelector("#all-parrots").textContent = "Ansaitut papukaijamerkit " + bagdecount + " / 5"
+
+    for (let i = 1; i <= bagdecount; i++) {
+        let parrotImg = document.querySelector(`#parrot-img-${i}`)
+        parrotImg.src = `./img/papukaija${i}.jpg`
+        parrotImg.classList.add("parrot-img")
+    }
 
     for (let i = 0; i < gameResults.length; i++) {
         if (gameResults[i].peli == "matikkapeli") {
@@ -24,5 +44,20 @@ function showRecords() {
         } else if (gameResults[i].peli == "tarinapeli") {
             storyResult.textContent = gameResults[i].tulos + " / 10"
         }
+    }
+}
+
+function countBadges() {
+
+    if (parrotBagdes > 0) {
+        document.querySelector("#parrot-img-one").src = "./img/papukaija1"
+    } if (parrotBagdes > 1) {
+        document.querySelector("#parrot-img-two").src = "./img/papukaija2"
+    } if (parrotBagdes > 2) {
+        document.querySelector("#parrot-img-three").src = "./img/papukaija3"
+    } if (parrotBagdes > 3) {
+        document.querySelector("#parrot-img-four").src = "./img/papukaija4"
+    } if (parrotBagdes > 4) {
+        document.querySelector("#parrot-img-five").src = "./img/papukaija5"
     }
 }
