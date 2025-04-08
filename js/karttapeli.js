e4f015ebc661e1a4726ab5f7dae2d5a8009881e9
nuum1 <noomiyi@gmail.com>
2025-04-08 17:48:01 +0300

    karttapelin js eteenpain

--- js/karttapeli.js
+++ js/karttapeli.js
@@ -1,11 +1,32 @@
 
 //QUESTIONS = IMAGES AND THEIR ANSWERS
 var questions =[
-    { src: " ", answer: "Italia"},
-    { src: " ", answer: "Intia"},
-    { src: " ", answer: "Etelä-Korea"},
-    { src: " ", answer: "Meksiko"},
-    { src: " ", answer: ""},
+    { src: " ", answer: "Italia",
+    hints: ["PIZZA", "Venetsia", "Jotai muuta siistiä"]},
+
+    { src: " ", answer: "Thaimaa",
+    hints: [" ", " ", " "]},
+
+    { src: " ", answer: "Algeria",
+    hints: [" ", " ", " "]},
+
+    { src: " ", answer: "Meksiko",
+    hints: [" ", " ", " "]},
+
+    { src: " ", answer: "Liettua",
+    hints: [" ", " ", " "]},
+
+    { src: " ", answer: "Brasilia",
+    hints: [" ", " ", " "]},
+
+    { src: " ", answer: "Papua-Uusi-Guinea",
+    hints: [" ", " ", " "]},
+
+    { src: " ", answer: "Portugali",
+    hints: [" ", " ", " "]},
+
+
+
 ];
 
  //current image number
@@ -14,19 +35,45 @@ var currentImage = 0;
 
 //function, current image and answer
 function loadImage() {
-    const question = questions[currentImage];
-    document.getElementById('image').src = image.src;
+    const image = questions[currentImage];
+    document.getElementById('image').src = image.src; //gets the image
+    document.getElementById("feedback").textContent= ""; //clears previous feedback
+    document.getElementById("answer").value = ""; //clears previous answer
 }
 
 
 
 //checks if the answer is right
 function checkAnswer() {
+    const userAnswer = document.getElementById("answer").value.toLowerCase();
+    const feedback = document.getElementById("feedback");
+    const correctAnswer = questions[currentImage].answer.toLowerCase(); //answer from the questions
+
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Oikein! Oikea vastaus oli: " + correctAnswer;
+    } else {
+        feedback.textContent = "Väärin. Oikea vastaus olisi ollut:  " + correctAnswer;
+    }
 
 }
 
 //goes to the next question
 function nextImage() {
-    currentImage += 1;
+    currentImage = (currentImage + 1) % questions.length
+  loadImage();  // Reload the new image
 }
 
+
+function showHint(index) {
+    const q=questions[currentImage];
+    const hintList = document.getElementById("hintList");
+
+    if (!document.getElementById(`hint-${index}`)) {
+        const li = document.createElement('li');
+        li.textContent = q.hints[index];
+        li.id = `hint-${index}`; // so it doesn’t show again
+        hintList.appendChild(li);
+      }
+}
+
+loadImage()
