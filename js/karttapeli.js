0062ae0d5c44e5a77fc1b1d3e34ffb75acd21cf9
nuum1 <noomiyi@gmail.com>
2025-04-12 20:55:32 +0300

    karttapelin muokkaus ver. 4

--- js/karttapeli.js
+++ js/karttapeli.js
@@ -1,7 +1,7 @@
 
 //QUESTIONS = IMAGES AND THEIR ANSWERS
 var questions =[
-    { src: "./img/papukaija1.jpg", answer: "Italia",
+    { src: "./img/NOOMITESTI_1.png", answer: "Italia",
     hints: ["Cappuccino", "Romulus ja Remus", "Pasta sekä pizza"]},
     { src: "./img/papukaija2.jpg", answer: "Thaimaa",
     hints: ["Suosittu matkailukohde Aasiassa", "Valuutta: baht ฿ (บาท)", "Suurimmat kaupungit pääkaupungin lisäksi: Samut Prakan, Udon Thani ja Chon Buri"]},
@@ -28,51 +28,83 @@ var questions =[
 
 ];
 
-
+//the number of questions
+let questionNumber = 9
 //current image number
-var currentImage = 0;
+let currentImage = 0;
 
 //current round
-var currentRound = 0;
+let currentRound = 0;
 
 //current points
-var currentPoint = 0;
+let currentPoint = 0;
 
 //current answer tries
-var answerTry = 0
+let answerTry = 3
 //function, current image and answer
 function loadImage() {
+    document.querySelector(".ohjeet_ja_peli").style.display = "block"
+ 
     const image = questions[currentImage];
     document.getElementById('image').src = image.src; //gets the image
     document.getElementById("feedback").textContent= ""; //clears previous feedback
     document.getElementById("answer").value = ""; //clears previous answer
     document.getElementById("hintList").textContent = ""; //clears hints
-    document.getElementById("tries").textContent= ""; //clears the tries
+    answerTry = 3
+    questionNumber --
+    
+    //ends the game when all the images are guessed
+    if (questionNumber === 0) {
+        document.querySelector(".ohjeet_ja_peli").style.display = "none"
+        document.querySelector(".pelin_tulokset").style.display = "block"
+    }
 }
-
 
 
 //checks if the answer is right
 function checkAnswer() {
-    const userAnswer = document.getElementById("answer").value.toLowerCase();
+    const userAnswer = document.getElementById("answer").value.toUpperCase();
     const feedback = document.getElementById("feedback");
-    const correctAnswer = questions[currentImage].answer.toLowerCase(); //answer from the questions
+    const correctAnswer = questions[currentImage].answer.toUpperCase(); //answer from the questions
 
-    if (userAnswer === correctAnswer) {
-        feedback.textContent = "Oikein! Oikea vastaus oli " + correctAnswer;
-    } else {
-        feedback.textContent = "Väärin: " + correctAnswer;
+    //FIGURE THIS OUT_____________________
+    if (userAnswer === " ") {
+        feedback.textContent = "Vastaus ei voi olla tyhjä."
+    }
+    //____________________________________
+
+    //the answer is right
+    if (userAnswer === correctAnswer) { 
+        feedback.textContent = "Oikein! Oikea vastaus oli " + correctAnswer + ". Sait " + "tosimonta" //TÄHÄN KUINKA MONTA PISTETTÄ
+        + ' pistettä/pisteen. Voit nyt jatkaa seuraavaan painamalla "Seuraava".';
     }
     
-    answerTry += 1
+    //the answer is wrong
+    if (userAnswer !== correctAnswer) {
 
+        //-1 tries
+        answerTry--
+    
+        //answer is wrong AND you still have guesses left
+        if (answerTry === 2) {
+            feedback.textContent = "Väärä vastaus. Sinulla on 2 (kaksi) yritystä jäljellä.";
+        }
+        if (answerTry === 1) {
+            feedback.textContent = "Väärä vastaus. Sinulla on 1 (yksi) yritys jäljellä.";
+        }
+
+        //answer is wrong AND your guesses are done
+        if (answerTry < 1) {
+            feedback.textContent = "Väärä vastaus. Oikea vastaus olisi ollut  " + correctAnswer + '. Et saanut tältä kierrokselta pisteitä. Voit nyt mennä seuraavaan kysymykseen painamalla "Seuraava"';
+
+        }       
+    }
 }
-
 
 //goes to the next question
 function nextImage() {
     currentImage = (currentImage + 1) % questions.length
-  loadImage();  // reloads next image
+    loadImage();  // reloads next image
 }
 
 
@@ -84,18 +116,15 @@ function showHint(index) {
     if (!document.getElementById(`hint-${index}`)) {
         const li = document.createElement('li');
         li.textContent = q.hints[index];
-        li.id = `hint-${index}`; // so it doesn’t show again
-        hintList.appendChild(li);
+        li.id = `hint-${index}`; //doesn't show the hint again
+        hintList.appendChild(li); 
       }
 }
 
+//updates the round number after every round
 function updateRound() {
     const currentRound = document.getElementById("kierrokset");
-    
-}
 
-function updateTries() {
-    
 }
 
 loadImage()
