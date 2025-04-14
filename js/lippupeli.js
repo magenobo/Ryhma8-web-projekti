4b898160a11f66e53bd324127c999c74ea09dec6
mstrandm <mstrandm24@students.oamk.fi>
2025-04-14 14:43:07 +0300

    Lippupelin html css ja js jatkaminen

--- js/lippupeli.js
+++ js/lippupeli.js
@@ -4,6 +4,11 @@ const aloita_peli_nappi = document.getElementById("aloita_peli")
 const aloita_alusta_nappi = document.getElementById("aloita_alusta")
 const katoa_div = document.getElementById("katoa")
 const peli_itse_div = document.getElementById("peli_itse")
+const kortin_taka_puoli = document.getElementById("taka_puoli")
+const kortin_etu_puoli = document.getElementById("etu_puoli")
+const kortti = document.getElementById("kortti_kuvat");
+
+peli_itse_div.style.display = "none";
 
 
 //kadottaa aloita peli nappulan ja kuvan
@@ -11,8 +16,7 @@ aloita_peli_nappi.addEventListener("click", () => {
     document.querySelector("#katoa").style.display = "none"
     
     //aloittaa pelin ja tuo sen esille
-    document.querySelector("peli_itse").style.display = "block"
-
+    peli_itse_div.style.display = "block";
 
 });
     
@@ -23,8 +27,14 @@ aloita_alusta_nappi.addEventListener("click", () => {
     document.getElementById("peli_itse").style.display = "none";
 });
 
+//Kun korttia klikkaa niin kortin takapuoli katoaa ja etupuoli tulee esiin
+kortin_taka_puoli.addEventListener("click", () => {
+    kortin_taka_puoli.style.display = "none";
+    kortin_etu_puoli.style.display = "block";
+});
 
-
-
+function kaanto() {
+    
+}
 
 
