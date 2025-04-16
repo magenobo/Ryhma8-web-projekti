eaa4f22dd8934049b78778fa8d47d583af7a5a59
MiciStrandman <mstrandm24@students.oamk.fi>
2025-04-16 23:57:48 +0300

    lippupelin html, css ja js lippujen kääntyminen

--- js/lippupeli.js
+++ js/lippupeli.js
@@ -1,40 +1,49 @@
 
 
-const aloita_peli_nappi = document.getElementById("aloita_peli")
-const aloita_alusta_nappi = document.getElementById("aloita_alusta")
-const katoa_div = document.getElementById("katoa")
-const peli_itse_div = document.getElementById("peli_itse")
-const kortin_taka_puoli = document.getElementById("taka_puoli")
-const kortin_etu_puoli = document.getElementById("etu_puoli")
-const kortti = document.getElementById("kortti_kuvat");
+document.addEventListener("DOMContentLoaded", () => {
 
-peli_itse_div.style.display = "none";
+    const aloita_peli_nappi = document.getElementById("aloita_peli")
+    const aloita_alusta_nappi = document.getElementById("aloita_alusta")
+    const katoa_div = document.getElementById("katoa")
+    const peli_itse_div = document.getElementById("peli_itse")
+    const kortin_taka_puoli = document.getElementById("taka_puoli")
+    const kortin_etu_puoli = document.getElementById("etu_puoli")
+    const kortti = document.getElementById("kortti_kuvat");
+
+    peli_itse_div.style.display = "none";
 
 
-//kadottaa aloita peli nappulan ja kuvan
-aloita_peli_nappi.addEventListener("click", () => {
-    document.querySelector("#katoa").style.display = "none"
+    //kadottaa aloita peli nappulan ja kuvan
+    aloita_peli_nappi.addEventListener("click", () => {
+        document.querySelector("#katoa").style.display = "none"
+        
+        //aloittaa pelin ja tuo sen esille
+        peli_itse_div.style.display = "block";
+
+    });
+        
+    //Aloita alusta napin toiminto
+    aloita_alusta_nappi.addEventListener("click", () => {
+
+        location.reload();
+    });
+
+
+
+    const card = document.querySelectorAll('.kortti')
+
+    card.forEach(kortti => {
+        kortti.addEventListener('click', () => {
+            kortti.classList.toggle('active')
+        });
+    });
+
     
-    //aloittaa pelin ja tuo sen esille
-    peli_itse_div.style.display = "block";
+
+    
+
+
+
+
 
 });
-    
-//
-aloita_alusta_nappi.addEventListener("click", () => {
-
-    document.getElementById("etusivu").querySelector("#katoa").style.display = "block" ;
-    document.getElementById("peli_itse").style.display = "none";
-});
-
-//Kun korttia klikkaa niin kortin takapuoli katoaa ja etupuoli tulee esiin
-kortin_taka_puoli.addEventListener("click", () => {
-    kortin_taka_puoli.style.display = "none";
-    kortin_etu_puoli.style.display = "block";
-});
-
-function kaanto() {
-    
-}
-
-
