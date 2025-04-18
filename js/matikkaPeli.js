373afe5b19fbc65c28ed4a35aeb2bbafc470ea6e
magenobo <johannes.manninen@hotmail.com>
2025-04-18 14:28:20 +0300

    Matikkapeli v 1.3: Koodin viimeisteleminen, aloitussivun lisääminen

--- js/matikkaPeli.js
+++ js/matikkaPeli.js
@@ -3,30 +3,35 @@ const ruoat = [
     {
         kuva: "./img/pasta.jpg",
         maa: "italia",
+        lore: "PASTA ON IHAN TÖRKEEN HYVÄÄ!",
         kysymys: "120 ml maitoa, kuinka monta kuppia se on?",
-        oikeaMuunnos: "0.5",
+        oikeaMuunnos: ["0.5", "1/2", "0,5"],
     },
     {
         kuva: "./img/macarons.jpg",
         maa: "ranska",
+        lore: "PASTA ON IHAN TÖRKEEN HYVÄÄ!",
         kysymys: "30 ml soijakastiketta, kuinka monta unssia (fl oz) se on?",
         oikeaMuunnos: "1",
     },
     {
         kuva: "./img/lihapullat.jpg",
         maa: "ruotsi",
+        lore: "PASTA ON IHAN TÖRKEEN HYVÄÄ!",
         kysymys: "1 kupin maitoa, kuinka monta millilitraa (ml) se on?",
         oikeaMuunnos: "240",
     },
     {
         kuva: "./img/wieninleike.jpg",
         maa: "itävalta",
+        lore: "PASTA ON IHAN TÖRKEEN HYVÄÄ!",
         kysymys: "1 kanafileen, joka painaa puolen naulan (lb) verran, montako grammaa (g) se on?",
         oikeaMuunnos: "227",
     },
     {
         kuva: "./img/omenapiiras.jpg",
-        maa: ["amerikka", "usa", "yhdysvallat"],
+        maa: "pohjois-amerikka",
+        lore: "PASTA ON IHAN TÖRKEEN HYVÄÄ!",
         kysymys: "yksi ja puoli kuppia sokeria, kuinka monta millilitraa (ml) se on?",
         oikeaMuunnos: "360",
     },
@@ -35,78 +40,69 @@ const ruoat = [
 // Globaali muuttujat
 let nykyinenIndeksi = 0;
 let pisteet = 0;
-let papukaijapisteet = 0;
 let peliData = JSON.parse(sessionStorage.getItem("pelit"))|| []
 
 
 // Haetaan elementit
+const alota = document.getElementById("aloitus");
 const kuvaElementti = document.querySelector(".kuvatahan img");
-const maaLomake = document.querySelectorAll("form")[0];
-const muunnosLomake = document.querySelectorAll("form")[1];
+const muunnosLomake = document.getElementById("muunnos");
+const valtio = document.getElementById("mikamikamaa")
 const maaInput = document.getElementById("tunnistus");
 const maaTunnistusNappi = document.getElementById("tunnistusNappi")
-const muunnosInput = document.getElementById("muunnos");
+const muunnosInput = document.getElementById("muunnosSubmit");
 const muunnosKysymys = document.getElementById("muunnosKysymys");
 const seuraavaKysymysNappi = document.getElementById("seuraavaKysymys");
 const pisteetElementti = document.querySelector(".pisteet");
 const griddyLaatikot = document.querySelectorAll(".griddy > div");
-
 
 function paivitaPeli() {
     const ruoka = ruoat[nykyinenIndeksi];
     kuvaElementti.src = ruoka.kuva;
     kuvaElementti.alt = "Kuva ruuasta";
     muunnosKysymys.textContent = ruoka.kysymys;
-    maaInput.value = "";
-    muunnosInput.value = "";
-    muunnosLomake.querySelectorAll("input").forEach(input => input.disabled = false);
-    maaInput.disabled = false;
-    maaInput.style.backgroundColor = "";
+    valtio.textContent = ruoka.maa;
+    muunnosLomake.value = "";
+    muunnosLomake.disabled = false;
+    muunnosInput.disabled = false;
+    muunnosLomake.style.backgroundColor = "white";
     muunnosInput.style.backgroundColor = "";
     seuraavaKysymysNappi.disabled = true; 
     seuraavaKysymysNappi.style.backgroundColor = "#ccc"; 
 }
 
 
-//vastaa valtiokysymyksestä
-maaLomake.addEventListener("submit", function (e) {
+alota.addEventListener("click", function (e){
     e.preventDefault();
-    const vastaus = maaInput.value.trim().toLowerCase();
-    const oikeatVastaukset = ruoat[nykyinenIndeksi].maa;
+    alert("penis")
+    alota.style.display = "none";
+)};
 
-    if (oikeatVastaukset.includes(vastaus)) {
-        papukaijapisteet++;
-        maaInput.style.backgroundColor = "#bdffbf";
-    } else {
-        maaInput.style.backgroundColor = "red";
-    }
-
-    maaInput.disabled = true;
-    maaTunnistusNappi.disabled = true;
-});
-
-//vastaa muunnoskysymyksestä
-muunnosLomake.addEventListener("submit", function (e) {
+//toivottavasti korjattu muunnoskysymys
+muunnosInput.addEventListener("click", function (e){
     e.preventDefault();
-    const vastaus = muunnosInput.value.trim();
+    const vastaus = muunnosLomake.value.trim();
     const oikeaVastaus = ruoat[nykyinenIndeksi].oikeaMuunnos;
 
-    if (vastaus === oikeaVastaus) {
+    if(oikeaVastaus.includes(vastaus)){
         pisteet++;
-        muunnosInput.style.backgroundColor = "#bdffbf";
+        muunnosLomake.style.backgroundColor = "#bdffbf";
         griddyLaatikot[nykyinenIndeksi].style.backgroundColor = "green";
         griddyLaatikot[nykyinenIndeksi].querySelector("img").setAttribute("src", "./img/oikein.png");
     } else {
-        muunnosInput.style.backgroundColor = "red";
+        muunnosLomake.style.backgroundColor = "red";
         griddyLaatikot[nykyinenIndeksi].style.backgroundColor = "red";
         griddyLaatikot[nykyinenIndeksi].querySelector("img").setAttribute("src", "./img/vaarin.png");
+    
     }
-
     seuraavaKysymysNappi.disabled = false; 
     seuraavaKysymysNappi.style.backgroundColor = "#cecece"; 
+    muunnosLomake.disabled = true;
 
-    muunnosLomake.querySelectorAll("input").forEach(input => input.disabled = true);
 });
+
+
+
 
 //nappi seuraavaan tehtävään
 seuraavaKysymysNappi.addEventListener("click", function () {
